ef9d596149cdd1bf10a491a05e674cfd17f25752
Hawkey <410898782@qq.com>
2018-01-16 11:16:33 +0800

    init

--- src/components/page-title.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-const style = require('../scss/page-title.scss');
-
-export default ( { title }: any ) => {
-    return (
-        <h2 className={style.title}>{title}<span /></h2>
-    );
-};
